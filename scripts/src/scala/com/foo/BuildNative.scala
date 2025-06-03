dcc82c17754db70d32f562d3617d1eb0fd2d93ff
lilac <cnjamesdeng@gmail.com>
2025-06-03 23:29:49 +0800

    Generate native image of JVM cli app

--- scripts/src/scala/com/foo/BuildNative.scala
+++ scripts/src/scala/com/foo/BuildNative.scala
@@ -5,20 +5,15 @@ import bleep.plugin.nativeimage.NativeImagePlugin
 
 object BuildNative extends BleepScript("build-native") {
   def run(started: Started, commands: Commands, args: List[String]): Unit = {
-    val projectName = model.CrossProjectName.fromString("native").get
+    val projectName = model.CrossProjectName.fromString("cli").get
     
     commands.compile(List(projectName))
-    
-    val targetPath = os.pwd / "foo-app"
-    
-    started.logger.info(s"Compiling native binary to $targetPath")
     
     val nativeImage = new NativeImagePlugin(
       project = started.bloopProject(projectName),
       logger = started.logger,
       jvmCommand = java.nio.file.Paths.get(System.getProperty("java.home"), "bin", "java"),
-      nativeImageOptions = List("-o", targetPath.toString()),
-      env = List.empty
+      env = List("PATH" -> System.getenv("PATH"))
     )
     
     val outputPath = nativeImage.nativeImage()
