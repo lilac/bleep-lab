d3e441f02514f6a4cbe22f14d8afaac1ce46dd9d
lilac <cnjamesdeng@gmail.com>
2025-06-04 08:05:16 +0800

    Add scalafmt configuration and format code

--- scripts/src/scala/com/foo/BuildNative.scala
+++ scripts/src/scala/com/foo/BuildNative.scala
@@ -6,18 +6,18 @@ import bleep.plugin.nativeimage.NativeImagePlugin
 object BuildNative extends BleepScript("build-native") {
   def run(started: Started, commands: Commands, args: List[String]): Unit = {
     val projectName = model.CrossProjectName.fromString("cli").get
-    
+
     commands.compile(List(projectName))
-    
+
     val nativeImage = new NativeImagePlugin(
       project = started.bloopProject(projectName),
       logger = started.logger,
       jvmCommand = java.nio.file.Paths.get(System.getProperty("java.home"), "bin", "java"),
       env = List("PATH" -> System.getenv("PATH"))
     )
-    
+
     val outputPath = nativeImage.nativeImage()
-    
+
     started.logger.info(s"Generated native binary at $outputPath")
   }
-} 
+}
